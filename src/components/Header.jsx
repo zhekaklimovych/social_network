44d09a81d58110dca09bec011756c407c0ef8ef3
Yevhenii Klimovych <yevhenii.k@coreb.in>
2022-03-20 20:26:49 +0200

    create base structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/c/c1/Steam_Logo.png'/>
+        </header>
+    )
+}
+
+export default Header;
