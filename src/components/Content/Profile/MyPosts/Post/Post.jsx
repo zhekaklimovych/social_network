8c01d8e16c1ec40c18e4b9a31682bc2e829aedb8
Yevhenii Klimovych <yevhenii.k@coreb.in>
2022-06-22 16:29:24 +0300

    add icon

--- src/components/Content/Profile/MyPosts/Post/Post.jsx
+++ src/components/Content/Profile/MyPosts/Post/Post.jsx
@@ -3,7 +3,7 @@ import s from './Post.module.css';
 const Post = (props) => {
     return(
         <div className={s.item}>
-            <img src="http://avatars.mitosa.net/monsterinc/avatar116.jpg" alt="" />
+            <img src="https://icons.iconarchive.com/icons/hopstarter/superhero-avatar/256/Avengers-Captain-America-icon.png" alt="" />
             {props.message}
             <button className={s.like}>Like</button>
       </div>
