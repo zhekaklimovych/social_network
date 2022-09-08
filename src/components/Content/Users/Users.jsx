45e6f4f0a78c100d03ca3b65cdc591ac4c4f27d6
Yevhenii Klimovych <y.klimovych@wpt.dev>
2022-09-08 23:24:16 +0300

    add users api component

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -1,65 +1,46 @@
 import s from './Users.module.css';
-import * as axios from 'axios';
 import React from 'react';
 import userPhoto from '../../assets/img/avatar.png';
 
-class Users extends React.Component {
 
-    componentDidMount() {
-        axios.get(`http://localhost:3001/api/users/?page=${this.props.currentPage}&limit=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.users);
-                this.props.setTotalUsersCount(response.data.totalCount);
-            });
-    }
-    
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`http://localhost:3001/api/users/?page=${pageNumber}&limit=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.users);
-            });
+const Users = (props) => {
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-        let pages = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-        return <div>
-
-            <div>
-                { pages.map( (p,id) => {
-                    return(
-                        <span 
-                            key={id} 
-                            className={this.props.currentPage === p ? s.selectedPage : null}
-                            onClick={() => { this.onPageChanged(p); }}
-                            >
+    return <div>
+        <div>
+            { pages.map( (p,id) => {
+                return(
+                    <span
+                        key={id}
+                        className={props.currentPage === p ? s.selectedPage : null}
+                        onClick={() => { props.onPageChanged(p); }}
+                    >
                                 {p}
-                        </span>) 
-                })}
-            </div>
-            {
-                this.props.users.map(u => <div key={u._id}>
+                        </span>)
+            })}
+        </div>
+        {
+            props.users.map(u => <div key={u._id}>
                     <span>
                         <div>
-                            <img src={userPhoto} className={s.userPhoto}/>
+                            <img src={userPhoto} className={s.userPhoto} alt={'avatar'}/>
                             <p>{u.name}</p>
                             <p>{u.email}</p>
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => { this.props.unfollow(u._id) }}>Unfollow</button>
-                                : <button onClick={() => { this.props.follow(u._id) }}>Follow</button>}
+                                ? <button onClick={() => { props.unfollow(u._id) }}>Unfollow</button>
+                                : <button onClick={() => { props.follow(u._id) }}>Follow</button>}
                         </div>
                     </span>
-                </div>)
-            }
-        </div>
-    }
+            </div>)
+        }
+    </div>
 }
 
 export default Users;
--- src/components/Content/Users/UsersContainer.jsx
+++ src/components/Content/Users/UsersContainer.jsx
@@ -1,6 +1,42 @@
 import Users from './Users';
 import {connect} from 'react-redux';
 import {followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC} from '../../../redux/actions/users_actions';
+import React from "react";
+import * as axios from "axios";
+
+class UsersContainerAPI extends React.Component {
+
+    componentDidMount() {
+        axios.get(`http://localhost:3002/api/users/?page=${this.props.currentPage}&limit=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.users);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`http://localhost:3002/api/users/?page=${pageNumber}&limit=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.users);
+            });
+    }
+
+    render() {
+        return <Users
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            users={this.props.users}
+            onPageChanged={this.onPageChanged}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+        />
+
+    }
+}
+
+
 
 let mapStateToProps = (state) => {
     return {
@@ -30,5 +66,5 @@ let mapDispatchToProps = (dispatch) => {
         }
     }
 }
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersContainerAPI);
 export default UsersContainer;
