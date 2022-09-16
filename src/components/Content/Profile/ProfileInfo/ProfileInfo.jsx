3f251bea7ced396f475b8a3b23e7f6d361dd396a
Yevhenii Klimovych <y.klimovych@wpt.dev>
2022-09-16 16:17:17 +0300

    add local state

--- src/components/Content/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Content/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,12 +1,16 @@
 import s from './ProfileInfo.module.css';
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
 
     return (
         <div className={s.profileInfo}>
             <div className={s.profileInfoLogo}>
+                <ProfileStatus status={'Hi my friends'}/>
+
                 <img src="https://templates.iqonic.design/socialv/bs5/html/dist/assets/images/user/11.png" alt=""/>
             </div>
+
             <div className={s.profileInfoDesc}>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam quam corrupti dolore officia dolores
                 aliquam culpa.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam quam corrupti dolore
---
+++ src/components/Content/Profile/ProfileInfo/ProfileStatus.jsx
@@ -0,0 +1,33 @@
+import {Component} from "react";
+
+class ProfileStatus extends Component {
+
+    state = {
+        editMode: false,
+        title: 'Yo'
+    }
+
+    activateEditMode() {
+        this.setState({editMode: true})
+    }
+    deactivateEditMode() {
+        this.setState({editMode: false})
+    }
+
+    render() {
+        return <div>
+            {!this.state.editMode &&
+                <div>
+                    <span onDoubleClick={this.activateEditMode.bind(this)} >{this.props.status}</span>
+                </div>
+            }
+            {this.state.editMode &&
+                <div>
+                    <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
+                </div>
+            }
+        </div>
+    }
+}
+
+export default ProfileStatus;
