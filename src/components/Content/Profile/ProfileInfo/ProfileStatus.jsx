07ecd696ff82aa7895f5f8d7ba1206f986ac2fd5
Yevhenii Klimovych <y.klimovych@wpt.dev>
2022-09-20 16:22:17 +0300

    add componentDidUpdate in status

--- src/components/Content/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Content/Profile/ProfileInfo/ProfileStatus.jsx
@@ -19,6 +19,11 @@ class ProfileStatus extends Component {
         this.setState({status: e.currentTarget.value})
     }
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(prevProps.status !== this.props.status) {
+            this.setState({status: this.props.status});
+        }
+    }
 
     render() {
         return <div>
