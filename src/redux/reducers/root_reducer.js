7b76e5a7f37a8106af037558ec7ebe54fa725857
Yevhenii Klimovych <y.klimovych@wpt.dev>
2022-10-15 00:14:29 +0300

    add form-reducer

--- src/redux/reducers/root_reducer.js
+++ src/redux/reducers/root_reducer.js
@@ -5,13 +5,15 @@ import dialogsReducer from './dialogs_reducer';
 import sidebarReducer from './sidebar_reducer';
 import usersReducer from './users_reducer';
 import auth_reducer from "./auth_reducer";
+import { reducer as formReducer } from 'redux-form';
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
     sidebar: sidebarReducer,
-    auth: auth_reducer
+    auth: auth_reducer,
+    form: formReducer
 })
 
 export default reducers;
